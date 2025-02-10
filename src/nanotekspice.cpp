5c6075b95a3827eaca95e6c821b3449b9452acbf
Antoine <me@antoinebellanger.fr>
2025-02-10 09:40:06 +0100

    feat: IComponent interface

---
+++ src/nanotekspice.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** nanotekspice
+** File description:
+** nanotekspice.cpp
+*/
+
+#include "IComponent.hpp"
+
+int main(const int argc, const char* argv)
+{
+    return 0;
+};
