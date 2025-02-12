7e0497b98c0719ca10c8813fe5eba07e889d2e94
Antoine <me@antoinebellanger.fr>
2025-02-12 19:50:49 +0100

    feat(Circuit): getter/upper for global tick value

--- src/Circuit.hpp
+++ src/Circuit.hpp
@@ -26,10 +26,14 @@ namespace nts
         public:
             void addComponent(const std::string& name, std::unique_ptr<IComponent> comp);
             [[nodiscard]] IComponent& getComponent(const std::string& name) const;
+
             listOf<components::special::OutputComponent> getOutputs() const { return this->_outputs; }
             listOf<components::special::InputComponent> getInputs() const { return this->_inputs; }
+            std::size_t getCurrentTick() const { return this->_curr_tick; }
+            std::size_t upCurrentTick() { return this->_curr_tick++; }
 
         private:
+            std::size_t _curr_tick = 0;
             std::unordered_map<std::string, std::unique_ptr<IComponent>> _components;
             listOf<components::special::OutputComponent> _outputs{};
             listOf<components::special::InputComponent> _inputs{};
