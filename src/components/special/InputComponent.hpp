40426e930f4b1277ced8e13719437211c541ce47
Antoine <me@antoinebellanger.fr>
2025-02-13 18:26:53 +0100

    feat(InputComponent): real simulate/compute
    now actually follow the demanded behavior

--- src/components/special/InputComponent.hpp
+++ src/components/special/InputComponent.hpp
@@ -18,14 +18,23 @@ namespace nts::components::special
             explicit InputComponent(): AComponent(PIN_NUMBER) {}
             ~InputComponent() override = default;
 
-            void simulate([[maybe_unused]] std::size_t tick) override {}
-            Tristate compute([[maybe_unused]] const std::size_t pin) override { return this->_currentPinStates[0]; }
+            void simulate([[maybe_unused]] std::size_t tick) override
+                { this->_currentPinStates.at(0) = this->newState; }
+            Tristate compute(const std::size_t pin) override
+            {
+                if (pin != PIN_NUMBER)
+                    throw Exceptions::UnknownPinException();
+                return this->_currentPinStates.at(0);
+            }
 
-            void setState(const Tristate state) { this->_currentPinStates[0] = state; }
+            void setState(const Tristate state) { this->newState = state; }
 
             [[nodiscard]] std::size_t getPinNumber() const override { return PIN_NUMBER; }
 
             constexpr static std::size_t PIN_NUMBER = 1;
+
+        protected:
+            Tristate newState = Tristate::Undefined;
     };
 }
 #endif //INPUTCOMPONENT_HPP
